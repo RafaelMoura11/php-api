d72b29bf2140889502d049f354b9a24622308c8b
Rafael <rafaelmoura7033.com>
2024-05-18 16:34:12 -0300

    First commit

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "meubanco";
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT user_id, name FROM users";
+$result = $conn->query($sql);
+
+$users = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+} 
+
+echo json_encode($users);
+
+$conn->close();
+?>
