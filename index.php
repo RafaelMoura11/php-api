77139c127baa3e81cb8adc528955d0c0edfa825e
Rafael <rafaelmoura7033.com>
2024-05-19 18:02:29 -0300

    Creating my first route

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 require_once __DIR__."/vendor/autoload.php";
+require_once __DIR__."/src/routes/main.php";
 
-print_r($_GET);
+use App\Core\Core;
+use App\Http\Route;
+
+Core::dispatch(Route::routes());
+
 ?>
---
+++ src/Core/Core.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Core;
+
+class Core {
+    public static function dispatch(array $routes) {
+        print_r($routes);
+    }
+}
