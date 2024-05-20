8c6c95cb72e9a4133714ad751e91a177cbcf3d54
Rafael <rafaelmoura7033.com>
2024-05-20 16:58:08 -0300

    Implementing update and delete user in User Service

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -56,7 +56,7 @@ class UserService {
 
     public static function fetch(mixed $authorization) {
         try {
-            if (isset($authorization['error'])) return ['error' => $authorization['error']];
+            if (isset($authorization['unauthorized'])) return ['error' => $authorization['error']];
 
             $userFromJWT = JWT::verify($authorization);
 
@@ -65,6 +65,59 @@ class UserService {
             $user = User::findAll();
             return $user;
         }
+        catch (PDOException $e) {
+            return ['error' => $e->getMessage()];
+        }
+        catch (\Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    public static function update(mixed $authorization, array $data) {
+        try {
+            if (isset($authorization['error'])) {
+                return ['unauthorized'=> $authorization['error']];
+            }
+
+            $userFromJWT = JWT::verify($authorization);
+
+            if (!$userFromJWT) return ['unauthorized'=> "Please, login to access this resource."];
+
+            $fields = Validator::validate([
+                'name' => $data['name'] ?? ''
+            ]);
+
+            $user = User::update($userFromJWT['id'], $fields);
+
+            if (!$user) return ['error'=> 'Sorry, we could not update your account.'];
+
+            return "User updated successfully!";
+        } 
+        catch (PDOException $e) {
+            return ['error' => $e->getMessage()];
+        }
+        catch (\Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    public static function delete(mixed $authorization, int|string $id)
+    {
+        try {
+            if (isset($authorization['error'])) {
+                return ['unauthorized'=> $authorization['error']];
+            }
+
+            $userFromJWT = JWT::verify($authorization);
+
+            if (!$userFromJWT) return ['unauthorized'=> "Please, login to access this resource."];
+
+            $user = User::delete($id);
+
+            if (!$user) return ['error'=> 'Sorry, we could not delete your account.'];
+
+            return "User deleted successfully!";
+        } 
         catch (PDOException $e) {
             return ['error' => $e->getMessage()];
         }
