9a9d143548babb8d47a9362dce89dc736e60cbe2
Rafael <rafaelmoura7033.com>
2024-05-20 15:01:55 -0300

    Implementing authentication method in Model User

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Database;
+use PDO;
 
 class User extends Database {
     public static function save(array $data) {
@@ -20,6 +21,34 @@ class User extends Database {
         ]);
 
         return $pdo->lastInsertId() > 0 ? true : false;
+    }
+
+    public static function authentication(array $data)
+    {
+        $pdo = self::getConnection();
+
+        $stmt = $pdo->prepare("
+            SELECT 
+                *
+            FROM 
+                users
+            WHERE 
+                email = ?
+        ");
+
+        $stmt->execute([$data['email']]);
+
+        if ($stmt->rowCount() < 1) return false;
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!password_verify($data['password'], $user['password'])) return false;
+
+        return [
+            'id'   => $user['id'],
+            'name' => $user['name'],
+            'email'=> $user['email'],
+        ];
     }
 }
 
