efc9253e4b7867d9f4b26bcbd1a3878909294060
Rafael <rafaelmoura7033.com>
2024-05-19 21:34:29 -0300

    Creating future routes and their methods

--- src/routes/main.php
+++ src/routes/main.php
@@ -1,7 +1,11 @@
 <?php
 use App\Http\Route;
 
-Route::get('/', 'HomeController@index');
-Route::get('/about', 'HomeController@index');
+Route::get('/',                     'HomeController@index');
+Route::post('/users/create',        'UserController@store');
+Route::post('/users/login',         'UserController@login');
+Route::get('/users/fetch',          'UserController@fetch');
+Route::get('/users/update',         'UserController@update');
+Route::get('/users/{id}/delete',    'UserController@remove');
 
 ?>
