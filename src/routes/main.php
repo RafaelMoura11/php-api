6c1854f7121ac1353a412124ca7e882597312ade
Rafael <rafaelmoura7033.com>
2024-05-20 16:57:38 -0300

    Fixing methods in some routes

--- src/routes/main.php
+++ src/routes/main.php
@@ -5,7 +5,7 @@ Route::get('/',                     'HomeController@index');
 Route::post('/users/create',        'UserController@store');
 Route::post('/users/login',         'UserController@login');
 Route::get('/users/fetch',          'UserController@fetch');
-Route::get('/users/update',         'UserController@update');
-Route::get('/users/{id}/delete',    'UserController@remove');
+Route::put('/users/update',         'UserController@update');
+Route::delete('/users/{id}/delete',    'UserController@remove');
 
 ?>
