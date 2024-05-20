af2fadb911c149de0ff6e30eb66d59bd47a51544
Rafael <rafaelmoura7033.com>
2024-05-20 17:27:19 -0300

    Creating fetch user by id route

--- src/routes/main.php
+++ src/routes/main.php
@@ -5,7 +5,8 @@ Route::get('/',                     'HomeController@index');
 Route::post('/users/create',        'UserController@store');
 Route::post('/users/login',         'UserController@login');
 Route::get('/users/fetch',          'UserController@fetch');
+Route::get('/users/fetch/{id}',     'UserController@fetchById');
 Route::put('/users/update',         'UserController@update');
-Route::delete('/users/{id}/delete',    'UserController@remove');
+Route::delete('/users/{id}/delete', 'UserController@remove');
 
 ?>
